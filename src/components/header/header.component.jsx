75bbd88f2c4e7b7bcd1b4465e3cd01a0bb1dcb19
Diwakar Parmar <diwakarparmar@gmail.com>
2020-01-18 00:00:17 -0500

    Updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -39,7 +39,7 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: { currentUser }, cart: {hidden }}) => ({
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
     currentUser,
     hidden
 });
